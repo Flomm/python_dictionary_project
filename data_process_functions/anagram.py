489e2aba5bf52c4f6eec7ed7430c9fc330a51cf6
Florian Baranya <florian.baranya@gmail.com>
2021-02-15 22:50:34 +0100

    Fix anagram function

--- data_process_functions/anagram.py
+++ data_process_functions/anagram.py
@@ -3,15 +3,17 @@ def anagram(word, text):
     output_list = []
 
     for expression in text:
-        expression_new = expression.lower()
-        if ' ' in expression_new:
-            expression_new = ''.join(expression_new.split(' '))
-        exp_list = list(expression_new)
-
-        for i in word:
-            if i in exp_list:
-                exp_list.remove(i)
-        if len(exp_list) == 0:
-            output_list.append(expression)
+        if expression.lower() != word:
+            expression_new = expression.lower()
+            if ' ' in expression_new:
+                expression_new = ''.join(expression_new.split(' '))
+            exp_list = list(expression_new)
+            test_word = list(word)
+            if len(exp_list) == len(test_word):
+                for i in word:
+                    if i in exp_list:
+                        test_word.remove(i)
+                if len(test_word) == 0:
+                    output_list.append(expression)
 
     return output_list
