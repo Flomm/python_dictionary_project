6fd04042baace9aefeaec6f56488c1bda132bf99
Florian Baranya <florian.baranya@gmail.com>
2021-02-15 22:51:13 +0100

    Update GUI and implement first functions

--- app.py
+++ app.py
@@ -4,35 +4,75 @@ import datetime
 from data_process_functions import starting_letter, final_letter, anagram, palindrom
 from file_handling_functions import process_input, write_output
 dicts_folder = 'E:/fbger/Flóri/Coding/Projects/python_dictionary/dicts/'
+output_folder = 'E:/fbger/Flóri/Coding/Projects/python_dictionary/outputs/'
 
 
-# def test():
-   
+def write_log(log_message):
+    log.configure(state='normal')
+    log.insert(
+        'end', f'{log_message}  {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}\n')
+    log.configure(state='disabled')
+
+
+def hasNumbers(inputString):
+    return any(char.isdigit() for char in inputString)
+
+
+def run_chosen_func():
+    if add_name.get() == '':
+        write_log('Please add a name for the file.')
+        return False
+    input_dict = process_input(dicts_drop.get())
+    if type(input_dict) is str:
+        write_log(input_dict)
+        return False
+
+    if choose_func.current() == 0:
+        if letter_input.get() == '':
+            write_log('Please add a word.')
+            return False
+        output_list = anagram(letter_input.get(), input_dict)
+    elif choose_func.current() == 1:
+        output_list = palindrom(input_dict)
+
+    result_log = write_output(output_list, add_name.get())
+    write_log(result_log)
+    file_name.set('')
+    letter_input.set('')
 
 
 window = tk.Tk()
 window.title('ReadDict')
-window.geometry('800x1200')
-window.configure(bg='grey')
+window.geometry('1100x500')
+window.configure(bg='#333B3F')
 
 file_name = tk.StringVar()
 letter_input = tk.StringVar()
 file_name.set('')
 letter_input.set('')
 
+left_frame = tk.LabelFrame(bg='#0078D7', bd=2)
+left_frame.pack(side='left', padx=20, pady=20)
+right_frame = tk.LabelFrame(bg='#0078D7', text='LOG')
+right_frame.pack(side='right', padx=20, pady=20)
 
-button = tk.Button(window, text='Enter', bg='blue', fg='white', command=test)
-add_name = tk.Entry(window, textvariable=file_name, width=10)
-file_label = tk.Label(window, text='Choose a name for the output file:')
-dicts_drop = ttk.Combobox(window, state='readonly', width=10)
-drop_label = tk.Label(window, text='Choose a dictionary:')
-choose_func = ttk.Combobox(window, state='readonly', width=25)
-func_label = tk.Label(window, text='Choose a function:')
-add_letter = tk.Entry(window, textvariable=letter_input, width=15)
+button = tk.Button(left_frame, text='Enter',
+                   bg='#333B3F', fg='white', command=run_chosen_func)
+add_name = tk.Entry(left_frame, textvariable=file_name, width=10)
+file_label = tk.Label(left_frame, text='Choose a name for the output file:')
+dicts_drop = ttk.Combobox(left_frame, state='readonly', width=10)
+drop_label = tk.Label(left_frame, text='Choose a dictionary:')
+choose_func = ttk.Combobox(left_frame, state='readonly', width=25)
+func_label = tk.Label(left_frame, text='Choose a function:')
+add_letter = tk.Entry(left_frame, textvariable=letter_input, width=15)
 letter_label = tk.Label(
-    window, text='Add a letter or word, according to chosen function.')
-log = tk.Text(window, state='disabled')
-log.insert(1.0, 'Log\n')
+    left_frame, text='Add a letter or word, according to chosen function:')
+log = tk.Text(right_frame, state='normal', width=50,
+              height=19, cursor='arrow', bg='#333B3F', fg='white')
+
+scroll_bar = ttk.Scrollbar(right_frame)
+scroll_bar.config(command=log.yview)
+log.config(yscrollcommand=scroll_bar.set)
 
 
 dicts_drop['values'] = ['English', 'German', 'Spanish', 'French']
